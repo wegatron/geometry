583c3057465802ac61e1f9179707c029af6af72d
wegatron <wegatron@gmail.com>
2015-06-03 15:41:08 +0800

    add res_analysis.cpp

---
+++ bilateral_normal_filtering/res_analysis.cpp
@@ -0,0 +1,13 @@
+#include "res_analysis.h"
+
+void zsw::analysisFaceNormalEnergy(const std::string &vtk_file, const std::string &output_vtk)
+{
+  std::cerr << "Function " << __FUNCTION__ << "in " << __FILE__ << __LINE__  << " haven't implement!!!" << std::endl;
+
+}
+
+void zsw::calFaceNormalEnergy(const std::multimap<size_t, size_t> &one_ring_f, const zjucad::matrix::matrix<double> &fnormal,
+                            zjucad::matrix::matrix<double> &energy)
+{
+  std::cerr << "Function " << __FUNCTION__ << "in " << __FILE__ << __LINE__  << " haven't implement!!!" << std::endl;
+}
