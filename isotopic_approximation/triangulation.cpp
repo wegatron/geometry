41982ee3d92bfa91392e373f5415920b0c0626d4
wegatron <wegatron@gmail.com>
2015-09-25 15:47:34 +0800

    clean code

--- isotopic_approximation/triangulation.cpp
+++ isotopic_approximation/triangulation.cpp
@@ -12,6 +12,18 @@ namespace zsw
 {
   TetMesh::TetMesh(const vector<Point> bz_points, const vector<Point> &bo_points, const vector<Point> &bi_points) : pf_(bz_points.size()*3, -1)
   {
+    // add points
+    for(const Point &pt : bz_points) {
+      tet_points_.push_back({0, pt, {}});
+    }
+    for(const Point &pt : bo_points) {
+      tet_points_.push_back({1, pt, {}});
+    }
+    for(const Point &pt : bi_points) {
+      tet_points_.push_back({-1, pt, {}});
+    }
+
+    // 3d triangulation
     std::vector<pair<Point, size_t>> tmo; // outer tetmesh and inner tetmesh
     size_t cnt = -1;
     for(const Point &pt : bz_points) {
@@ -29,20 +41,11 @@ namespace zsw
 
     Delaunay to(tmo.begin(), tmo.end());
     Delaunay ti(tmi.begin(), tmi.end());
-    // fill data
-    for(const Point &pt : bz_points) {
-      tet_points_.push_back({0, pt, {}});
-    }
-    for(const Point &pt : bo_points) {
-      tet_points_.push_back({1, pt, {}});
-    }
-    for(const Point &pt : bi_points) {
-      tet_points_.push_back({-1, pt, {}});
-    }
 
     ASSURE(to.is_valid(), exit(__LINE__));
     ASSURE(ti.is_valid(), exit(__LINE__));
 
+    // add tets
     size_t tet_id = -1;
     for(Delaunay::Finite_cells_iterator cit=to.finite_cells_begin();
         cit!=to.finite_cells_end(); ++cit) {
@@ -67,23 +70,25 @@ namespace zsw
       tet_points_[p_ids[3]].tet_ids_.push_back(tet_id);
       // @todo take sample points
     }
-
-    std::cerr << "cell num:" << tet_id << std::endl;
     std::cerr << " @TODO take sample points in the tet!" << std::endl;
-    std::cerr << "@TODO add edges!" << std::endl;
-    // // edges
-    // for(Delaunay::Finite_edges_iterator eit=to.finite_edges_begin();
-    //     eit!=to.finite_edges_end(); ++eit) {
-    //   // pair<size_t, size_t> edge(eit->vertex(0)->info(), eit->vertex(1)->info());
-    //   // if(edge.first > edge.second) { swap(edge.first, edge.second); }
-    //   edges_.push_back(pair<size_t, size_t>(eit->second, eit->third));
-    //   //std::cerr << "@TODO check edge is half edge or not!" << std::endl;
-    // }
+
+    // add edges
+    for(Delaunay::Finite_edges_iterator eit=to.finite_edges_begin();
+        eit!=to.finite_edges_end(); ++eit) {
+      edges_.push_back(pair<size_t, size_t>(eit->second, eit->third));
+    }
+
+    for(Delaunay::Finite_edges_iterator eit=ti.finite_edges_begin();
+        eit!=ti.finite_edges_end(); ++eit) {
+      if(tet_points_[eit->first].point_type_==0 && tet_points_[eit->second].point_type_==0) { continue; }
+      edges_.push_back(pair<size_t, size_t>(eit->second, eit->third));
+    }
   }
 
   void TetMesh::simplify()
   {
     std::cerr << "Function " << __FUNCTION__ << "in " << __FILE__ << __LINE__  << " haven't implement!!!" << std::endl;
+
   }
 
   void TetMesh::cleanPoints()
