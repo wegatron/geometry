1a38d48c1446bb47342405228c07df105963aea6
wegatron <wegatron@gmail.com>
2015-10-08 19:59:15 +0800

    update edge collapse data structure

--- isotopic_approximation/triangulation.cpp
+++ isotopic_approximation/triangulation.cpp
@@ -14,35 +14,22 @@ using namespace std;
 namespace zsw
 {
   TetMesh::TetMesh(const vector<Point> bz_points, const vector<Point> &bo_points, const vector<Point> &bi_points,
-                   const zsw::Scalar sample_dense) : pf_(bz_points.size()*3, -1)
+                   const zsw::Scalar sample_dense)
   {
     sample_dense_ = sample_dense;
     // add points
-    for(const Point &pt : bz_points) {
-      tet_points_.push_back({0, pt, {}});
-    }
-    for(const Point &pt : bo_points) {
-      tet_points_.push_back({1, pt, {}});
-    }
-    for(const Point &pt : bi_points) {
-      tet_points_.push_back({-1, pt, {}});
-    }
+    for(const Point &pt : bz_points) { vertices_.push_back({0, pt, -1, {}}); }
+    for(const Point &pt : bo_points) { vertices_.push_back({1, pt, -1, {}}); }
+    for(const Point &pt : bi_points) {  vertices_.push_back({-1, pt, -1, {}}); }
 
     // 3d triangulation
     std::vector<pair<Point, size_t>> tmo; // outer tetmesh and inner tetmesh
     size_t cnt = -1;
-    for(const Point &pt : bz_points) {
-      tmo.push_back(pair<Point, size_t>(pt, ++cnt));
-    }
+    for(const Point &pt : bz_points) {  tmo.push_back(pair<Point, size_t>(pt, ++cnt)); }
     vector<pair<Point, size_t>> tmi(tmo);
 
-    for(const Point &pt : bo_points) {
-      tmo.push_back(pair<Point, size_t>(pt, ++cnt));
-    }
-
-    for(const Point &pt : bi_points) {
-      tmi.push_back(pair<Point, size_t>(pt, ++cnt));
-    }
+    for(const Point &pt : bo_points) { tmo.push_back(pair<Point, size_t>(pt, ++cnt)); }
+    for(const Point &pt : bi_points) { tmi.push_back(pair<Point, size_t>(pt, ++cnt)); }
 
     Delaunay to(tmo.begin(), tmo.end());
     Delaunay ti(tmi.begin(), tmi.end());
@@ -51,234 +38,206 @@ namespace zsw
     ASSURE(ti.is_valid(), exit(__LINE__));
 
     // add tets
-    size_t tet_id = -1;
-    for(Delaunay::Finite_cells_iterator cit=to.finite_cells_begin();
-        cit!=to.finite_cells_end(); ++cit) {
-      size_t p_ids[4] = {cit->vertex(0)->info(), cit->vertex(1)->info(), cit->vertex(2)->info(), cit->vertex(3)->info()};
-      tets_.push_back({{p_ids[0], p_ids[1], p_ids[2], p_ids[3]}, {}});
-      ++tet_id;
-      tet_points_[p_ids[0]].tet_ids_.push_back(tet_id);
-      tet_points_[p_ids[1]].tet_ids_.push_back(tet_id);
-      tet_points_[p_ids[2]].tet_ids_.push_back(tet_id);
-      tet_points_[p_ids[3]].tet_ids_.push_back(tet_id);
+    size_t tet_id=0;
+    addTets(to, tet_id);
+    //std::cerr << "[INFO] stage 1 finished! tet size:" << tet_id << std::endl;
+    addTets(ti, tet_id);
+    addEdges(ti,to);
+  }
 
-      //take sample points
-      // static zsw::common::ClockC11 clock;
-      // static int count = 0;
-      // count++;
-      // if(count%100 == 0) {       std::cerr << "cost:" << clock.time() << std::endl;      }
-      Eigen::Matrix<zsw::Scalar, 3, 4> sample_tet_points;
-      for(size_t i=0; i<4; ++i) {
-        sample_tet_points(0,i) = tet_points_[p_ids[i]].pt_data_[0];
-        sample_tet_points(1,i) = tet_points_[p_ids[i]].pt_data_[1];
-        sample_tet_points(2,i) = tet_points_[p_ids[i]].pt_data_[2];
-      }
-      zsw::sampleTet(sample_dense_, sample_tet_points, tets_.back().sample_points_);
-    }
-
-    std::cout << "[INFO] stage 1 finished! tet size:" << tet_id << std::endl;
-
-    for(Delaunay::Finite_cells_iterator cit=ti.finite_cells_begin();
-        cit!=ti.finite_cells_end(); ++cit) {
-      size_t p_ids[4] = {cit->vertex(0)->info(), cit->vertex(1)->info(), cit->vertex(2)->info(), cit->vertex(3)->info()};
-      tets_.push_back({{p_ids[0], p_ids[1], p_ids[2], p_ids[3]}, {}});
-      ++tet_id;
-      tet_points_[p_ids[0]].tet_ids_.push_back(tet_id);
-      tet_points_[p_ids[1]].tet_ids_.push_back(tet_id);
-      tet_points_[p_ids[2]].tet_ids_.push_back(tet_id);
-      tet_points_[p_ids[3]].tet_ids_.push_back(tet_id);
-
-      // take sample_points
-      Eigen::Matrix<zsw::Scalar, 3, 4> sample_tet_points;
-      for(size_t i=0; i<4; ++i) {
-        sample_tet_points(0,i) = tet_points_[p_ids[i]].pt_data_[0];
-        sample_tet_points(1,i) = tet_points_[p_ids[i]].pt_data_[1];
-        sample_tet_points(2,i) = tet_points_[p_ids[i]].pt_data_[2];
-      }
-      zsw::sampleTet(sample_dense_, sample_tet_points, tets_.back().sample_points_);
-    }
-
+  void TetMesh::addEdges(const Delaunay &ti, const Delaunay &to)
+  {
     // add edges
     for(Delaunay::Finite_edges_iterator eit=to.finite_edges_begin();
         eit!=to.finite_edges_end(); ++eit) {
-      edges_.push_back(pair<size_t, size_t>(eit->second, eit->third));
+      edges_.push_back({eit->second, eit->third});
     }
 
     for(Delaunay::Finite_edges_iterator eit=ti.finite_edges_begin();
         eit!=ti.finite_edges_end(); ++eit) {
-      if(tet_points_[eit->second].point_type_==0 && tet_points_[eit->third].point_type_==0) { continue; }
-      edges_.push_back(pair<size_t, size_t>(eit->second, eit->third));
+      if(vertices_[eit->second].pt_type_==0 && vertices_[eit->third].pt_type_==0) { continue; }
+      edges_.push_back({eit->second, eit->third});
+    }
+
+    for(Edge & te : edges_) {
+      set<size_t> te_fv;
+      set<size_t> tet_id_set;
+      vector<size_t> tet_ids;
+      for(size_t tet_id : vertices_[te.vind0_].tet_ids_) { tet_id_set.insert(tet_id); }
+      for(size_t tet_id : vertices_[te.vind1_].tet_ids_) {
+        if(tet_id_set.find(tet_id)!=tet_id_set.end()) { tet_ids.push_back(tet_id); }
+      }
+
+      for(size_t tet_id : tet_ids) {
+        Tet &tt=tets_[tet_id];
+        if(tt.vind0_ != te.vind0_ && tt.vind0_ !=te.vind1_) { te_fv.insert(tt.vind0_); }
+        if(tt.vind1_ != te.vind0_ && tt.vind1_ !=te.vind1_) { te_fv.insert(tt.vind1_); }
+        if(tt.vind2_ != te.vind0_ && tt.vind2_ !=te.vind1_) { te_fv.insert(tt.vind2_); }
+        if(tt.vind3_ != te.vind0_ && tt.vind3_ !=te.vind1_) { te_fv.insert(tt.vind3_); }
+      }
+      te.fv_.resize(te_fv.size());
+      copy(te_fv.begin(), te_fv.end(), te.fv_.begin());
+    }
+  }
+  void TetMesh::addTets(const Delaunay &td, size_t &tet_id)
+  {
+    for(Delaunay::Finite_cells_iterator cit=td.finite_cells_begin();
+        cit!=td.finite_cells_end(); ++cit) {
+      Tet tmp_tet = {cit->vertex(0)->info(), cit->vertex(1)->info(), cit->vertex(2)->info(), cit->vertex(3)->info()};
+      tets_.push_back(tmp_tet);
+      vertices_[tmp_tet.vind0_].tet_ids_.push_back(tet_id);
+      vertices_[tmp_tet.vind1_].tet_ids_.push_back(tet_id);
+      vertices_[tmp_tet.vind2_].tet_ids_.push_back(tet_id);
+      vertices_[tmp_tet.vind3_].tet_ids_.push_back(tet_id);
+
+      // static zsw::common::ClockC11 clock;
+      // static int count = 0;
+      // count++;
+      // if(count%100 == 0) {       std::cerr << "cost:" << clock.time() << std::endl;      }
+
+      //take sample points
+      Eigen::Matrix<zsw::Scalar, 3, 4> sample_tet_points;
+      sample_tet_points(0,0) = vertices_[tmp_tet.vind0_].pt_[0];
+      sample_tet_points(1,0) = vertices_[tmp_tet.vind0_].pt_[1];
+      sample_tet_points(2,0) = vertices_[tmp_tet.vind0_].pt_[2];
+
+      sample_tet_points(0,1) = vertices_[tmp_tet.vind1_].pt_[0];
+      sample_tet_points(1,1) = vertices_[tmp_tet.vind1_].pt_[1];
+      sample_tet_points(2,1) = vertices_[tmp_tet.vind1_].pt_[2];
+
+      sample_tet_points(0,2) = vertices_[tmp_tet.vind2_].pt_[0];
+      sample_tet_points(1,2) = vertices_[tmp_tet.vind2_].pt_[1];
+      sample_tet_points(2,2) = vertices_[tmp_tet.vind2_].pt_[2];
+
+      sample_tet_points(0,3) = vertices_[tmp_tet.vind3_].pt_[0];
+      sample_tet_points(1,3) = vertices_[tmp_tet.vind3_].pt_[1];
+      sample_tet_points(2,3) = vertices_[tmp_tet.vind3_].pt_[2];
+
+      // add sample points into sample_points_
+      zsw::sampleTet(sample_dense_, sample_tet_points, sample_points_);
+      ++tet_id;
     }
   }
 
-  void TetMesh::simplify()
-  {
-    // collapse ZEdges
-    bool collapsable = true;
-    while(collapsable) {
-      collapsable = false;
-      // here the edges_'s vector size should not change
-      for(pair<size_t,size_t> &edge : edges_) {
-        if(tet_points_[edge.first].point_type_==0 && tet_points_[edge.second].point_type_==0) {
-          if(collapseZEdge(edge)) { collapsable=true; break; }
+
+    void TetMesh::simplify()
+    {
+      // collapse ZEdges
+      bool collapsable = true;
+      while(collapsable) {
+        collapsable = false;
+        // here the edges_'s vector size should not change
+        for(Edge &edge : edges_) {
+          for(; vertices_[edge.vind0_].father_!=-1; edge.vind0_=vertices_[edge.vind0_].father_);
+          for(; vertices_[edge.vind1_].father_!=-1; edge.vind1_=vertices_[edge.vind1_].father_);
+          if(edge.vind0_==edge.vind1_) { continue; }
+          if(vertices_[edge.vind0_].pt_type_==0 && vertices_[edge.vind1_].pt_type_==0) {
+            if(collapseZEdge(edge)) { collapsable=true; break; }
+          }
         }
       }
     }
+
+    bool TetMesh::collapseZEdge(Edge &edge)
+    {
+      // for(; pf_[edge.first]!=-1; edge.first=pf_[edge.first]);
+      // for(; pf_[edge.second]!=-1; edge.second=pf_[edge.second]);
+
+      // // using clean tets
+      // std::vector<size_t> tet_ids;
+      // for(size_t id : tet_points_[edge.first].tet_ids_) {
+      //   if(isValidTet(tets_[id])) {     tet_ids.push_back(id);      }
+      // }
+      // tet_points_[edge.first].tet_ids_=tet_ids;
+
+      // tet_ids.clear();
+      // for(size_t id : tet_points_[edge.second].tet_ids_) {
+      //   if(isValidTet(tets_[id])) {     tet_ids.push_back(id);      }
+      // }
+      // tet_points_[edge.second].tet_ids_=tet_ids;
+
+      // // kernel_region_judge
+      // KernelRegionJudger krj;
+      // for(size_t id : tet_points_[edge.first].tet_ids_) {
+      //   updateKrj(id);
+      // }
+      // for(size_t id : tet_points_[edge.second].tet_ids_) {
+      //   updateKrj(id);
+      // }
+
+      // bool isfind=false;
+      // Point ret_pt;
+      // for(size_t id : tet_points_[edge.first].tet_ids_) {
+      //   for(Point pt : tets_[id].sample_points_) {
+      //     if(krj.judge(pt)) { isfind=true; ret_pt=pt; break; }
+      //   }
+      // }
+      // vector<int> new_tet_ids;
+      // if(!isfind) {
+      //   for(size_t id : tet_points_[edge.second].tet_ids_) {
+      //     new_tet_ids.push_back(id);
+      //     for(Point pt : tets_[id].sample_points_) {
+      //       if(krj.judge(pt)) { isfind=true; ret_pt=pt; break; }
+      //     }
+      //   }
+      // }
+
+      // if(isfind) {      // resolve
+      //   tet_points_[edge.first].point_type_=0;
+      //   tet_points_[edge.first].pt_data_=ret_pt;
+      //   pf_[edge.second]=edge.first;
+      // }
+      // return isfind;
+      return false;
+    }
+
+    void TetMesh::writeVtk(const std::string &filepath)
+    {
+//       std::ofstream ofs;
+//       OPEN_STREAM(filepath, ofs, std::ofstream::out, return);
+//       std::vector<zsw::Scalar> pts_data;
+//       std::vector<size_t> tets_data;
+//       cleanPoints();
+//       for(const TetPoint &tet_point : tet_points_) {
+//         pts_data.push_back(tet_point.pt_data_[0]);
+//         pts_data.push_back(tet_point.pt_data_[1]);
+//         pts_data.push_back(tet_point.pt_data_[2]);
+//       }
+
+// #ifdef DEBUG
+//       // add sampling points in the tet
+//       for(Tet &tet : tets_) {
+//         if(!isValidTet(tet)) { continue; }
+//         if(tet_points_[tet.pt_ids_[0]].point_type_==tet_points_[tet.pt_ids_[1]].point_type_ &&
+//            tet_points_[tet.pt_ids_[1]].point_type_==tet_points_[tet.pt_ids_[2]].point_type_ &&
+//            tet_points_[tet.pt_ids_[2]].point_type_==tet_points_[tet.pt_ids_[3]].point_type_) { continue; } // useless tet the same type
+//         for(const Point &pt : tet.sample_points_) {
+//           pts_data.push_back(pt[0]);
+//           pts_data.push_back(pt[1]);
+//           pts_data.push_back(pt[2]);
+//         }
+//       }
+// #endif
+//       for(Tet &tet : tets_) {
+//         if(!isValidTet(tet)) { continue; }
+//         if(tet_points_[tet.pt_ids_[0]].point_type_==tet_points_[tet.pt_ids_[1]].point_type_ &&
+//            tet_points_[tet.pt_ids_[1]].point_type_==tet_points_[tet.pt_ids_[2]].point_type_ &&
+//            tet_points_[tet.pt_ids_[2]].point_type_==tet_points_[tet.pt_ids_[3]].point_type_) { continue; } // useless tet the same type
+
+//         tets_data.push_back(tet.pt_ids_[0]);
+//         tets_data.push_back(tet.pt_ids_[1]);
+//         tets_data.push_back(tet.pt_ids_[2]);
+//         tets_data.push_back(tet.pt_ids_[3]);
+//       }
+//       size_t n_pts = pts_data.size()/3;
+//       size_t n_tets = tets_data.size()/4;
+//       std::cout << "[INFO] point size:" << n_pts << std::endl;
+//       std::cout << "[INFO] tet size:" << n_tets << std::endl;
+//       tet2vtk(ofs, &pts_data[0], n_pts, &tets_data[0], n_tets);
+    }
+
+    void TetMesh::writeZeroSetSurface(const std::string &filepath)
+    {
+      std::cerr << "Function " << __FUNCTION__ << "in " << __FILE__ << __LINE__  << " haven't implement!!!" << std::endl;
+    }
   }
-
-  bool TetMesh::collapseZEdge(std::pair<size_t,size_t> &edge)
-  {
-    for(; pf_[edge.first]!=-1; edge.first=pf_[edge.first]);
-    for(; pf_[edge.second]!=-1; edge.second=pf_[edge.second]);
-
-    // using clean tets
-    std::vector<size_t> tet_ids;
-    for(size_t id : tet_points_[edge.first].tet_ids_) {
-      if(isValidTet(tets_[id])) {     tet_ids.push_back(id);      }
-    }
-    tet_points_[edge.first].tet_ids_=tet_ids;
-
-    tet_ids.clear();
-    for(size_t id : tet_points_[edge.second].tet_ids_) {
-      if(isValidTet(tets_[id])) {     tet_ids.push_back(id);      }
-    }
-    tet_points_[edge.second].tet_ids_=tet_ids;
-
-    // kernel_region_judge
-    KernelRegionJudger krj;
-    for(size_t id : tet_points_[edge.first].tet_ids_) {
-      updateKrj(id);
-    }
-    for(size_t id : tet_points_[edge.second].tet_ids_) {
-      updateKrj(id);
-    }
-
-    bool isfind=false;
-    Point ret_pt;
-    for(size_t id : tet_points_[edge.first].tet_ids_) {
-      for(Point pt : tets_[id].sample_points_) {
-        if(krj.judge(pt)) { isfind=true; ret_pt=pt; break; }
-      }
-    }
-    vector<int> new_tet_ids;
-    if(!isfind) {
-      for(size_t id : tet_points_[edge.second].tet_ids_) {
-        new_tet_ids.push_back(id);
-        for(Point pt : tets_[id].sample_points_) {
-          if(krj.judge(pt)) { isfind=true; ret_pt=pt; break; }
-        }
-      }
-    }
-
-    if(isfind) {      // resolve
-      tet_points_[edge.first].point_type_=0;
-      tet_points_[edge.first].pt_data_=ret_pt;
-      pf_[edge.second]=edge.first;
-    }
-    return isfind;
-  }
-
-  void TetMesh::cleanPoints()
-  {
-    // set new index
-    std::vector<TetPoint> n_tet_points;
-    std::vector<size_t> index(tet_points_.size(), -1);
-    size_t id = -1;
-    for(size_t i=0; i<pf_.size(); ++i) {
-      if(pf_[i] == -1) {
-        index[i] = ++id;
-        n_tet_points.push_back(tet_points_[i]);
-      }
-    }
-
-    std::vector<size_t> id_map(tet_points_.size(), -1);
-    for(size_t i=0; i<pf_.size(); ++i) {
-      size_t tmp = i;
-      while(pf_[tmp] != -1) { tmp = pf_[tmp]; }
-      id_map[i] = index[tmp];
-    }
-
-    // using id_map to update edges and tets
-    for(Tet &tet : tets_) {
-      for(size_t i=0; i<4; ++i) {
-        tet.pt_ids_[i] = id_map[tet.pt_ids_[i]];
-      }
-    }
-
-    for(std::pair<size_t, size_t> &edge : edges_) {
-      edge.first = id_map[edge.first];
-      edge.second = id_map[edge.second];
-    }
-
-    tet_points_ = n_tet_points;
-    pf_.resize(tet_points_.size());
-    fill(pf_.begin(), pf_.end(), -1);
-  }
-
-  void TetMesh::writeVtk(const std::string &filepath)
-  {
-    std::ofstream ofs;
-    OPEN_STREAM(filepath, ofs, std::ofstream::out, return);
-    std::vector<zsw::Scalar> pts_data;
-    std::vector<size_t> tets_data;
-    cleanPoints();
-    for(const TetPoint &tet_point : tet_points_) {
-      pts_data.push_back(tet_point.pt_data_[0]);
-      pts_data.push_back(tet_point.pt_data_[1]);
-      pts_data.push_back(tet_point.pt_data_[2]);
-    }
-
-    #ifdef DEBUG
-    // add sampling points in the tet
-    for(Tet &tet : tets_) {
-      if(!isValidTet(tet)) { continue; }
-      if(tet_points_[tet.pt_ids_[0]].point_type_==tet_points_[tet.pt_ids_[1]].point_type_ &&
-         tet_points_[tet.pt_ids_[1]].point_type_==tet_points_[tet.pt_ids_[2]].point_type_ &&
-         tet_points_[tet.pt_ids_[2]].point_type_==tet_points_[tet.pt_ids_[3]].point_type_) { continue; } // useless tet the same type
-      for(const Point &pt : tet.sample_points_) {
-        pts_data.push_back(pt[0]);
-        pts_data.push_back(pt[1]);
-        pts_data.push_back(pt[2]);
-      }
-    }
-    #endif
-    for(Tet &tet : tets_) {
-      if(!isValidTet(tet)) { continue; }
-      if(tet_points_[tet.pt_ids_[0]].point_type_==tet_points_[tet.pt_ids_[1]].point_type_ &&
-         tet_points_[tet.pt_ids_[1]].point_type_==tet_points_[tet.pt_ids_[2]].point_type_ &&
-         tet_points_[tet.pt_ids_[2]].point_type_==tet_points_[tet.pt_ids_[3]].point_type_) { continue; } // useless tet the same type
-
-      tets_data.push_back(tet.pt_ids_[0]);
-      tets_data.push_back(tet.pt_ids_[1]);
-      tets_data.push_back(tet.pt_ids_[2]);
-      tets_data.push_back(tet.pt_ids_[3]);
-    }
-    size_t n_pts = pts_data.size()/3;
-    size_t n_tets = tets_data.size()/4;
-    std::cout << "[INFO] point size:" << n_pts << std::endl;
-    std::cout << "[INFO] tet size:" << n_tets << std::endl;
-    tet2vtk(ofs, &pts_data[0], n_pts, &tets_data[0], n_tets);
-  }
-
-  void TetMesh::writeZeroSetSurface(const std::string &filepath)
-  {
-    std::cerr << "Function " << __FUNCTION__ << "in " << __FILE__ << __LINE__  << " haven't implement!!!" << std::endl;
-  }
-
-  bool TetMesh::isValidTet(Tet &tet)
-  {
-    for(size_t i=0; i<4; ++i) {
-      for(; pf_[tet.pt_ids_[i]]!=-1; tet.pt_ids_[i]=pf_[tet.pt_ids_[i]]);
-    }
-    sort(&(tet.pt_ids_[0]), &(tet.pt_ids_[0])+4);
-    return (tet.pt_ids_[0]!=tet.pt_ids_[1]) &&  (tet.pt_ids_[1]!=tet.pt_ids_[2])
-      &&  (tet.pt_ids_[2]!=tet.pt_ids_[3]);
-  }
-
-  bool TetMesh::isValidEdge(pair<size_t, size_t> &edge)
-  {
-    for(; pf_[edge.first]!=-1; edge.first=pf_[edge.first]);
-    for(; pf_[edge.second]!=-1; edge.second=pf_[edge.second]);
-    return edge.first!=edge.second;
-  }
-}
--- isotopic_approximation/triangulation.h
+++ isotopic_approximation/triangulation.h
@@ -2,6 +2,7 @@
 #define TRIANGULATION_H
 
 #include <vector>
+#include <memory>
 #include <Eigen/Dense>
 #include <zswlib/mesh/mesh_type.h>
 
@@ -9,55 +10,68 @@
 
 namespace zsw {
 
-  class KernelRegionJudger
-  {
-  public:
-    KernelRegionJudger();
-    void addConstraint(const Eigen::Matrix<zsw::Scalar,3,1> &v0, const Eigen::Matrix<zsw::Scalar,3,1> &v1,
-                       const Eigen::Matrix<zsw::Scalar,3,1> &v2, const Eigen::Matrix<zsw::Scalar,3,1> &vr);
-    bool judge(const Point &pt);
-  private:
-    std::vector<Eigen::Matrix<zsw::Scalar,3,1>> vec_v0;
-    std::vector<Eigen::Matrix<zsw::Scalar,3,1>> vec_v1;
-    std::vector<Eigen::Matrix<zsw::Scalar,3,1>> vec_v2;
-    std::vector<Eigen::Matrix<zsw::Scalar,3,1>> vec_vr;
-  };
+  /* class KernelRegionJudger */
+  /* { */
+  /* public: */
+  /*   KernelRegionJudger(); */
+  /*   void addConstraint(const Eigen::Matrix<zsw::Scalar,3,1> &v0, const Eigen::Matrix<zsw::Scalar,3,1> &v1, */
+  /*                      const Eigen::Matrix<zsw::Scalar,3,1> &v2, const Eigen::Matrix<zsw::Scalar,3,1> &vr); */
+  /*   bool judge(const Point &pt); */
+  /* private: */
+  /*   std::vector<Eigen::Matrix<zsw::Scalar,3,1>> vec_v0; */
+  /*   std::vector<Eigen::Matrix<zsw::Scalar,3,1>> vec_v1; */
+  /*   std::vector<Eigen::Matrix<zsw::Scalar,3,1>> vec_v2; */
+  /*   std::vector<Eigen::Matrix<zsw::Scalar,3,1>> vec_vr; */
+  /* }; */
+
   class TetMesh
   {
   public:
-    struct TetPoint
+    struct Vertex
     {
-      char point_type_; // outer_boundary 1, zero_set 0, inner_boundary -1
-      Point pt_data_;
+      char pt_type_; // outer_boundary 1, zero_set 0, inner_boundary -1
+      Point pt_;
+      size_t father_;
       std::vector<size_t> tet_ids_;
     };
 
     struct Tet
     {
-      size_t pt_ids_[4]; // sorted
-      std::vector<Point> sample_points_;
+      size_t vind0_;
+      size_t vind1_;
+      size_t vind2_;
+      size_t vind3_;
+    };
+
+    struct Edge
+    {
+      size_t vind0_;
+      size_t vind1_;
+      std::vector<size_t> fv_; // the vertex construct a face with this edge
     };
 
     TetMesh(const std::vector<Point> bz_points, const std::vector<Point> &bo_points, const std::vector<Point> &bi_points,
             const zsw::Scalar sample_dense);
 
-    /*** clean invalid points
-     * update tets and edges(keep invalid tets and edges)
-     */
-    void cleanPoints();
     void simplify();
     void writeVtk(const std::string &filepath);
     void writeZeroSetSurface(const std::string &filepath);
   private:
-    void updateKrj(size_t tet_id);
-    bool collapseZEdge(std::pair<size_t,size_t> &edge);
-    bool isValidTet(Tet &tet);
-    bool isValidEdge(std::pair<size_t, size_t> &edge);
+    void addTets(const Delaunay &td, size_t &tet_id);
+    void addEdges(const Delaunay &ti, const Delaunay &to);
+    bool collapseZEdge(Edge &edge);
+    bool isValidEdge(Edge &edge)
+    {
+      for(; vertices_[edge.vind0_].father_!=-1; edge.vind0_=vertices_[edge.vind0_].father_);
+      for(; vertices_[edge.vind1_].father_!=-1; edge.vind1_=vertices_[edge.vind1_].father_);
+      return edge.vind0_ != edge.vind1_;
+    }
+
     zsw::Scalar sample_dense_;
+    std::vector<Vertex> vertices_;
     std::vector<Tet> tets_;
-    std::vector<TetPoint> tet_points_;
-    std::vector<std::pair<size_t, size_t>> edges_;
-    std::vector<size_t> pf_;
+    std::vector<Edge> edges_;
+    std::vector<Point> sample_points_;
   };
 }
 
