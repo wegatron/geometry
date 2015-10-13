77e827f99c792b69e41a0e3cd87b9cfca32e9a2b
wegatron <wegatron@gmail.com>
2015-10-13 13:32:15 +0800

    collapse_edge checked seems fine!

--- isotopic_approximation/test/test_collapseEdge.cpp
+++ isotopic_approximation/test/test_collapseEdge.cpp
@@ -14,7 +14,17 @@ void calcFv(const zsw::TetMesh::Edge &edge, const vector<zsw::TetMesh::Vertex> &
             const vector<zsw::TetMesh::Tet> &tets,
             set<size_t> &fv)
 {
-  std::cerr << "Function " << __FUNCTION__ << "in " << __FILE__ << __LINE__  << " haven't implement!!!" << std::endl;
+  fv.clear();
+  for(size_t tet_id : vertices[edge.vind0_].tet_ids_) {
+    if(!tets[tet_id].valid_) { continue; }
+    if(tets[tet_id].vind0_==edge.vind1_ || tets[tet_id].vind1_==edge.vind1_
+       || tets[tet_id].vind2_==edge.vind1_ || tets[tet_id].vind3_==edge.vind1_) {
+      if(tets[tet_id].vind0_ != edge.vind0_ && tets[tet_id].vind0_!=edge.vind1_) { fv.insert(tets[tet_id].vind0_); }
+      if(tets[tet_id].vind1_ != edge.vind0_ && tets[tet_id].vind1_!=edge.vind1_) { fv.insert(tets[tet_id].vind1_); }
+      if(tets[tet_id].vind2_ != edge.vind0_ && tets[tet_id].vind2_!=edge.vind1_) { fv.insert(tets[tet_id].vind2_); }
+      if(tets[tet_id].vind3_ != edge.vind0_ && tets[tet_id].vind3_!=edge.vind1_) { fv.insert(tets[tet_id].vind3_); }
+    }
+  }
 }
 
 BOOST_AUTO_TEST_CASE(collapse_edge_fkr0)
@@ -35,6 +45,7 @@ BOOST_AUTO_TEST_CASE(collapse_edge_fkr0)
 
   vector<size_t> invalid_tet_ids;
   vector<size_t> updated_tet_ids;
+  set<size_t> invalid_edge_ids;
   const size_t vind0=67;
   const size_t vind1=62;
   for(size_t tet_id : vertices[vind1].tet_ids_) {
@@ -42,6 +53,15 @@ BOOST_AUTO_TEST_CASE(collapse_edge_fkr0)
        || tets[tet_id].vind2_==vind0 || tets[tet_id].vind3_==vind0) {
       invalid_tet_ids.push_back(tet_id);
     } else {      updated_tet_ids.push_back(tet_id);    }
+  }
+
+  for(size_t eid : vertices[vind1].edge_ids_) {
+    if(edges[eid].vind0_==vind0 || edges[eid].vind1_==vind0) { continue; }
+    size_t tvind=(edges[eid].vind0_!=vind1) ? edges[eid].vind0_ : edges[eid].vind1_;
+    for(size_t eeid : vertices[tvind].edge_ids_) {
+      // check vertex tvind not link vind0
+      if(edges[eeid].vind0_==vind0 || edges[eeid].vind1_==vind0) {        invalid_edge_ids.insert(eid);      }
+    }
   }
 
   tm.writeVtk("/home/wegatron/tmp/tmp_before.vtk");
@@ -60,20 +80,23 @@ BOOST_AUTO_TEST_CASE(collapse_edge_fkr0)
   for(size_t tet_id : invalid_tet_ids) {    BOOST_CHECK(!tets[tet_id].valid_);   }
   for(size_t tet_id : updated_tet_ids) {
     BOOST_CHECK_MESSAGE(
-    tets[tet_id].vind0_==vind0 || tets[tet_id].vind1_==vind0 || tets[tet_id].vind2_==vind0 || tets[tet_id].vind3_==vind0,
+                        tets[tet_id].vind0_==vind0 || tets[tet_id].vind1_==vind0 || tets[tet_id].vind2_==vind0 || tets[tet_id].vind3_==vind0,
                         std::to_string(tets[tet_id].vind0_)+","+std::to_string(tets[tet_id].vind0_)+","
-    +std::to_string(tets[tet_id].vind0_)+","+std::to_string(tets[tet_id].vind0_));
+                        +std::to_string(tets[tet_id].vind0_)+","+std::to_string(tets[tet_id].vind0_));
   }
+
   // check edges' vind
   size_t tmp_cnt=0;
   for(size_t eid : vertices[vind1].edge_ids_) {
     if(edges[eid].vind0_==vind1 || edges[eid].vind1_==vind1) { BOOST_CHECK(!edges[eid].valid_); ++tmp_cnt; }
-    else { BOOST_CHECK(edges[eid].vind0_==vind0 || edges[eid].vind1_==vind0); }
+    else if(edges[eid].valid_) {
+      BOOST_CHECK(invalid_edge_ids.find(eid) == invalid_edge_ids.end());
+      BOOST_CHECK(edges[eid].vind0_==vind0 || edges[eid].vind1_==vind0);
+    } else {        BOOST_CHECK(invalid_edge_ids.find(eid) != invalid_edge_ids.end());      }
   }
   BOOST_CHECK(tmp_cnt==1);
   /// check edges' fv and fv_cnt
   // check each edge linked by adjacent vertex's fv is right
-
   set<size_t> vids;
   for(size_t tet_id : updated_tet_ids) {
     if(tets[tet_id].vind0_!=vind0) { vids.insert(tets[tet_id].vind0_); }
@@ -87,7 +110,8 @@ BOOST_AUTO_TEST_CASE(collapse_edge_fkr0)
       if(!edges[eid].valid_) { continue; }
       set<size_t> fv;
       calcFv(edges[eid], vertices, tets, fv);
-      BOOST_CHECK(fv.size() == edges[eid].fv_cnt_);
+      BOOST_CHECK_MESSAGE(fv.size() == edges[eid].fv_cnt_, "fv.size()="+std::to_string(fv.size())
+                          +" which should be "+std::to_string(edges[eid].fv_cnt_));
       bool flag=true;
       size_t tfv_cnt=0;
       for(size_t tfv : edges[eid].fv_) {
@@ -102,4 +126,4 @@ BOOST_AUTO_TEST_CASE(collapse_edge_fkr0)
   tm.writeVtk("/home/wegatron/tmp/tmp_after.vtk");
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+  BOOST_AUTO_TEST_SUITE_END()
