602699b84fac6edd2ba75f3756ca39ee99f6af60
wegatron <wegatron@gmail.com>
2015-10-27 09:35:50 +0800

    update last commit

--- isotopic_approximation/test/test_optimizer.cpp
+++ isotopic_approximation/test/test_optimizer.cpp
@@ -11,40 +11,40 @@ using namespace Ipopt;
 
 BOOST_AUTO_TEST_SUITE(Inequality_equations_optimizer)
 
-// BOOST_AUTO_TEST_CASE(optimizer_basic)
-// {
-//   Eigen::Matrix<Ipopt::Number,3,1> cx; cx<<1,1,1;
-//   SmartPtr<zsw::Optimizer> mynlp = new zsw::Optimizer(cx);
+BOOST_AUTO_TEST_CASE(optimizer_basic)
+{
+  Eigen::Matrix<Ipopt::Number,3,1> cx; cx<<1,1,1;
+  SmartPtr<zsw::Optimizer> mynlp = new zsw::Optimizer(cx);
 
-//   mynlp->addConstraint(1,0,0,0);
-//   mynlp->addConstraint(0,1,0,0);
-//   mynlp->addConstraint(0,0,1,0);
-//   mynlp->addConstraint(-1,-1,-1,-1);
+  mynlp->addConstraint(1,0,0,0);
+  mynlp->addConstraint(0,1,0,0);
+  mynlp->addConstraint(0,0,1,0);
+  mynlp->addConstraint(-1,-1,-1,-1);
 
-//   SmartPtr<IpoptApplication> app = IpoptApplicationFactory();
+  SmartPtr<IpoptApplication> app = IpoptApplicationFactory();
 
-//   app->Options()->SetNumericValue("tol", 1e-3);
-//   app->Options()->SetStringValue("mu_strategy", "adaptive");
-//   app->Options()->SetStringValue("output_file", "ipopt.out");
+  app->Options()->SetNumericValue("tol", 1e-3);
+  app->Options()->SetStringValue("mu_strategy", "adaptive");
+  app->Options()->SetStringValue("output_file", "ipopt.out");
 
-//   ApplicationReturnStatus status;
-//   status = app->Initialize();
-//   if (status != Solve_Succeeded) {
-//     printf("\n\n*** Error during initialization!\n");
-//     return;
-//   }
+  ApplicationReturnStatus status;
+  status = app->Initialize();
+  if (status != Solve_Succeeded) {
+    printf("\n\n*** Error during initialization!\n");
+    return;
+  }
 
-//   // Ask Ipopt to solve the problem
-//   status = app->OptimizeTNLP(mynlp);
-//   if (status == Solve_Succeeded) {
-//     printf("\n\n*** The problem solved!\n");
-//   }
-//   else {
-//     printf("\n\n*** The problem FAILED!\n");
-//   }
-//   BOOST_REQUIRE(status == Solve_Succeeded);
-//   BOOST_REQUIRE(mynlp->verify()==true);
-// }
+  // Ask Ipopt to solve the problem
+  status = app->OptimizeTNLP(mynlp);
+  if (status == Solve_Succeeded) {
+    printf("\n\n*** The problem solved!\n");
+  }
+  else {
+    printf("\n\n*** The problem FAILED!\n");
+  }
+  BOOST_REQUIRE(status == Solve_Succeeded);
+  BOOST_REQUIRE(mynlp->verify()==true);
+}
 
 BOOST_AUTO_TEST_CASE(optimizer_adv0)
 {
