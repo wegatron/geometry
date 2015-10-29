ba06b3a7e1986e8886413148d1720dd4bcc6e2a9
wegatron <wegatron@gmail.com>
2015-10-29 21:56:32 +0800

    analysis bad 3d delaunay triangulation

--- isotopic_approximation/surface_generator.cpp
+++ isotopic_approximation/surface_generator.cpp
@@ -1,7 +1,8 @@
+#include <OpenMesh/Core/IO/MeshIO.hh>
 #include "surface_generator.h"
 
 #include <iostream>
-
+#define ZSW_DEBUG
 // void zsw::SurfaceGenerator::genSurface(const zsw::Scalar dis, zsw::mesh::TriMesh &tm,
 //                                        zsw::mesh::TriMesh &bo_mesh, zsw::mesh::TriMesh &bi_mesh)
 // {
@@ -19,24 +20,37 @@
 // }
 
 void zsw::genPoints(const zsw::Scalar dis,
-                                      zsw::mesh::TriMesh &tm,
-                                      std::vector<Delaunay::Point> &bz_points,
-                                      std::vector<Delaunay::Point> &bo_points,
-                                      std::vector<Delaunay::Point> &bi_points)
+                    zsw::mesh::TriMesh &tm,
+                    std::vector<Delaunay::Point> &bz_points,
+                    std::vector<Delaunay::Point> &bo_points,
+                    std::vector<Delaunay::Point> &bi_points)
 {
   if(!tm.has_vertex_normals()) {
     tm.request_face_normals();
     tm.request_vertex_normals();
     tm.update_normals();
   }
+#ifdef ZSW_DEBUG
+  zsw::mesh::TriMesh tmo(tm);
+  zsw::mesh::TriMesh tmi(tm);
+#endif
   for(zsw::mesh::TriMesh::ConstVertexIter vit=tm.vertices_begin(); vit!=tm.vertices_end(); ++vit) {
     Eigen::Matrix<zsw::Scalar, 3, 1> ep = tm.point(*vit);
     Eigen::Matrix<zsw::Scalar, 3, 1> offset = tm.normal(*vit) * dis;
     Delaunay::Point bz_p(ep[0], ep[1], ep[2]);
     Delaunay::Point bo_p(ep[0]+offset[0], ep[1]+offset[1], ep[2]+offset[2]);
     Delaunay::Point bi_p(ep[0]-offset[0], ep[1]-offset[1], ep[2]-offset[2]);
+#ifdef ZSW_DEBUG
+    tmi.set_point(zsw::mesh::TriMesh::VertexHandle(vit->idx()), ep-offset);
+    tmo.set_point(zsw::mesh::TriMesh::VertexHandle(vit->idx()), ep+offset);
+#endif
     bz_points.push_back(bz_p);
     bo_points.push_back(bo_p);
     bi_points.push_back(bi_p);
   }
+
+#ifdef ZSW_DEBUG
+  OpenMesh::IO::write_mesh(tmi, "/home/wegatron/tmp/gen_point_tmi.obj");
+  OpenMesh::IO::write_mesh(tmo, "/home/wegatron/tmp/gen_point_tmo.obj");
+#endif
 }
--- isotopic_approximation/test/test_tetmesh.cpp
+++ isotopic_approximation/test/test_tetmesh.cpp
@@ -16,7 +16,7 @@ void test_triangulation(const string &file_path, const string &output_prefix, co
   zsw::genPoints(dis, input_mesh, bz_points, bo_points, bi_points);
 
   zsw::TetMesh tm(bz_points, bo_points, bi_points);
-  tm.writeVtk(output_prefix+"out.vtk");
+  tm.writeVtk(output_prefix+"out.vtk",-1);
   tm.writeVtk(output_prefix+"in.vtk", 1);
   std::cerr << "!!!!!!!" << std::endl;
 }
@@ -24,12 +24,12 @@ void test_triangulation(const string &file_path, const string &output_prefix, co
 int main(int argc, char *argv[])
 {
   test_triangulation("/home/wegatron/workspace/geometry/data/teaport.obj",
-                     "/home/wegatron/tmp/test_triangulation/teaport_", 0.01);
+                     "/home/wegatron/tmp/test_triangulation/teaport_", 0.04);
 
-  test_triangulation("/home/wegatron/workspace/geometry/data/beam.stl",
-                     "/home/wegatron/tmp/test_triangulation/beam_", 0.1);
+  // test_triangulation("/home/wegatron/workspace/geometry/data/beam.stl",
+  //                    "/home/wegatron/tmp/test_triangulation/beam_", 0.1);
 
-  test_triangulation("/home/wegatron/workspace/geometry/data/monkey.stl",
-                     "/home/wegatron/tmp/test_triangulation/monkey_", 0.03);
+  // test_triangulation("/home/wegatron/workspace/geometry/data/monkey.stl",
+  //                    "/home/wegatron/tmp/test_triangulation/monkey_", 0.03);
   return 0;
 }
