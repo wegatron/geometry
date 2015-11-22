e3c11868192f8f00e6c5a5f2f101640008d32b04
wegatron <wegatron@gmail.com>
2015-11-22 16:13:45 +0800

    update

--- isotopic_approximation/basic_op.cpp
+++ isotopic_approximation/basic_op.cpp
@@ -1,5 +1,6 @@
 #include "basic_op.h"
 
+#if 0
 zsw::Scalar zsw::calcPoint2TriSquaredDis(const Eigen::Matrix<zsw::Scalar,3,1> &pt,
                                          const Eigen::Matrix<zsw::Scalar,3,1> &v0,
                                          const Eigen::Matrix<zsw::Scalar,3,1> &v1,
@@ -56,4 +57,55 @@ zsw::Scalar zsw::calcPoint2TriSquaredDis(const Eigen::Matrix<zsw::Scalar,3,1> &p
   } else {
     return ((project_p-v2).cross(nc)).squaredNorm()+project_f_square_dis;
   }
+}
+#endif
+
+
+zsw::Scalar zsw::calcPoint2TriSquaredDis(const Eigen::Matrix<zsw::Scalar,3,1> &pt,
+                                         const Eigen::Matrix<zsw::Scalar,3,1> &v0,
+                                         const Eigen::Matrix<zsw::Scalar,3,1> &v1,
+                                         const Eigen::Matrix<zsw::Scalar,3,1> &v2)
+{
+  // calc the projection point
+  Eigen::Matrix<zsw::Scalar,3,2> A;
+  A.block<3,1>(0,0)=v1-v0; A.block<3,1>(0,1)=v2-v0;
+  Eigen::Matrix<zsw::Scalar,2,3> AT = A.transpose();
+  Eigen::PartialPivLU<Eigen::Matrix<zsw::Scalar,2,2>> pplu;
+  pplu.compute(AT*A);
+  Eigen::Matrix<zsw::Scalar,2,1> ans = pplu.solve(AT*(pt-v0));
+  if(ans[0]>0 && ans[1]>0 && ans[0]+ans[1]<1) {
+    return (pt-v0 - A*ans).squaredNorm();
+  }
+
+  // init ret
+  zsw::Scalar ret=min((pt-v0).squaredNorm(), (pt-v1).squaredNorm());
+  ret = min(ret, (pt-v2).squaredNorm());
+
+  {    // project to v0 v1
+    Eigen::Matrix<zsw::Scalar,3,1> a = v1-v0;
+    Eigen::Matrix<zsw::Scalar,3,1> na = a.normalize();
+    zsw::Scalar jv=(pt-v0).dot(na);
+    if(jv>0 && jv<a.norm()) { // on segement v0 v1
+      ret = min(ret, ((pt-v0).cross(na)).squaredNorm());
+    }
+  }
+
+  {   // project to v0 v2
+    Eigen::Matrix<zsw::Scalar,3,1> b = v2-v0;
+    Eigen::Matrix<zsw::Scalar,3,1> nb = b.normalize();
+    zsw::Scalar jv=(pt-v0).dot(nb);
+    if(jv>0 && jv<b.norm()) {
+      ret = min(ret, ((pt-v0).cross(nb)).squaredNorm());
+    }
+  }
+
+  {  // project to v1 v2
+    Eigen::Matrix<zsw::Scalar,3,1> c = v2-v1;
+    Eigen::Matrix<zsw::Scalar,3,1> nc = c.normalize();
+    zsw::Scalar jv=(pt-v1).dot(nc);
+    if(jv>0 && jv<c.norm()) {
+      ret = min(ret, ((pt-v1).cross(nc)).squaredNorm());
+    }
+  }
+  return ret;
 }
