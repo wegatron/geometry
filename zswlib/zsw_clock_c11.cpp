ad08c9806df4ef3efcb1f9c5d2414fa32bdea7fd
wegatron <wegatron@gmail.com>
2016-03-09 10:06:21 +0800

    add class TimeAnalysis

---
+++ zswlib/zsw_clock_c11.cpp
@@ -0,0 +1,8 @@
+#include "zsw_clock_c11.h"
+
+zsw::common::TimeAnalysis ta;
+
+zsw::common::TimeAnalysis & zsw::common::getTA()
+{
+  return ta;
+}
--- zswlib/zsw_clock_c11.h
+++ zswlib/zsw_clock_c11.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <chrono>
+#include <map>
 
 namespace zsw {
   namespace common {
@@ -18,15 +19,51 @@ namespace zsw {
         cur_time_ =  std::chrono::high_resolution_clock::now();
         return std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(cur_time_ - pre_time).count());
       }
+
+      size_t timeCount() {
+        TimePoint pre_time = cur_time_;
+        cur_time_ =  std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(cur_time_ - pre_time).count();
+      }
+
+      size_t totalTimeCount() {
+        cur_time_ = std::chrono::high_resolution_clock::now();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(cur_time_ - start_time_).count();
+      }
+
       std::string totalTime() {
         cur_time_ = std::chrono::high_resolution_clock::now();
         return std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(cur_time_ - start_time_).count());
       }
     private:
+      ClockC11(const ClockC11 &clock) = delete;
       TimePoint start_time_;
       TimePoint cur_time_;
     };
+
+    class TimeAnalysis
+    {
+    public:
+      TimeAnalysis() {}
+      size_t getTime(const std::string &name) const {
+        auto it = time_cost_map_.find(name);
+        if(it == time_cost_map_.end()) { return 0; }
+        return it->second;
+      }
+
+      void setTime(const std::string &name, size_t ms) {
+        time_cost_map_[name] = ms;
+      }
+    private:
+      TimeAnalysis(const TimeAnalysis &ta) = delete;
+      std::map<std::string, size_t> time_cost_map_; // cost function - millisecond
+    };
+
+    TimeAnalysis &getTA();
   }
 }
+
+#define GET_TA(name) zsw::common::getTA().getTime(name)
+#define SET_TA(name, ms) zsw::common::getTA().setTime(name, ms)
 
 #endif /* ZSW_CLOCK_C11_H */
