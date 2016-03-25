ba68fb3bea37df4b5c9dfb6aa4efe47cc7c39597
wegatron <wegatron@gmail.com>
2016-03-25 17:24:19 +0800

    update makefile and output zerosurf obj file in v0

--- isotopic_approximation2/app/main.cpp
+++ isotopic_approximation2/app/main.cpp
@@ -12,14 +12,42 @@
 
 using namespace std;
 
-void appro_nowv(
-                const size_t version,
-                const std::string &ori_file_path,
-                const std::string &deformed_file_path,
+void appro_v0(const std::string &ori_file_path,
                 const std::string &output_dir,
                 const zsw::Scalar err_epsilon,
                 const zsw::Scalar tri_sample_r,
                 const zsw::Scalar tet_sample_r)
+{
+  RESETLOG_FILE(output_dir+"log");
+  zsw::mesh::TriMesh input_mesh;
+  if(!OpenMesh::IO::read_mesh(input_mesh, ori_file_path)) {
+    std::cerr << "can't open file " << ori_file_path << std::endl;
+    abort();
+  }
+  std::vector<Eigen::Matrix<zsw::Scalar,3,1>> inner_jpts;
+  std::vector<Eigen::Matrix<zsw::Scalar,3,1>> outer_jpts;
+  std::vector<Eigen::Matrix<zsw::Scalar,3,1>> bs_jpts;
+  zsw::genAndSampleShell(input_mesh, err_epsilon, tri_sample_r, inner_jpts, outer_jpts, bs_jpts);
+  zsw::writePoints(output_dir+"ori_inner_jpts.vtk", inner_jpts);
+  zsw::writePoints(output_dir+"ori_outer_jpts.vtk", outer_jpts);
+  zsw::writePoints(output_dir+"ori_bs_jpts.vtk", bs_jpts);
+  zsw::Approximation appro;
+  appro.setTmpOutDir(output_dir);
+  appro.init(err_epsilon, tri_sample_r, tet_sample_r, inner_jpts, outer_jpts, bs_jpts);
+  appro.writeTetMesh(output_dir+"refine_res.vtk", {zsw::ignore_bbox, zsw::ignore_self_in, zsw::ignore_self_out});
+  appro.simp(output_dir);
+  appro.writeTetMesh(output_dir+"simped_final.vtk", {zsw::ignore_bbox, zsw::ignore_out});
+  appro.writeZeroSurface(output_dir+"simped_final.obj");
+}
+
+void appro_v1(
+              const size_t version,
+              const std::string &ori_file_path,
+              const std::string &deformed_file_path,
+              const std::string &output_dir,
+              const zsw::Scalar err_epsilon,
+              const zsw::Scalar tri_sample_r,
+              const zsw::Scalar tet_sample_r)
 {
   RESETLOG_FILE(output_dir+"log");
   zsw::mesh::TriMesh input_mesh;
@@ -49,7 +77,7 @@ void appro_nowv(
   zsw::writePoints(output_dir+"ori_bs_jpts.vtk", bs_jpts);
   zsw::Approximation appro;
   appro.setGscale(g_scale);
-  appro.setTetByRef(version == 2);
+  //appro.setTetByRef(version == 2);
   appro.setTmpOutDir(output_dir);
   appro.initD(err_epsilon, tri_sample_r, tet_sample_r, inner_jpts, outer_jpts, bs_jpts,
               deformed_inner_jpts, deformed_outer_jpts, deformed_bs_jpts);
@@ -58,37 +86,11 @@ void appro_nowv(
   appro.writeTetMesh(output_dir+"simp_final_tol.vtk", {zsw::ignore_bbox, zsw::ignore_self_out});
   appro.writeTetMesh(output_dir+"simp_final_deformed_tol.vtk", {zsw::ignore_bbox, zsw::ignore_self_out}, nullptr, false);
   appro.writeTetMesh(output_dir+"simped_final.vtk", {zsw::ignore_bbox, zsw::ignore_out});
-  appro.writeTetMesh(output_dir+"simped_final_deformed.vtk", {zsw::ignore_bbox, zsw::ignore_out}, nullptr, false);
+  //appro.writeTetMesh(output_dir+"simped_final_deformed.vtk", {zsw::ignore_bbox, zsw::ignore_out}, nullptr, false);
+  appro.writeZeroSurface(output_dir+"simped_final.obj");
 }
 
-void appro_oriv(const std::string &ori_file_path,
-                const std::string &output_dir,
-                const zsw::Scalar err_epsilon,
-                const zsw::Scalar tri_sample_r,
-                const zsw::Scalar tet_sample_r)
-{
-  RESETLOG_FILE(output_dir+"log");
-  zsw::mesh::TriMesh input_mesh;
-  if(!OpenMesh::IO::read_mesh(input_mesh, ori_file_path)) {
-    std::cerr << "can't open file " << ori_file_path << std::endl;
-    abort();
-  }
-  std::vector<Eigen::Matrix<zsw::Scalar,3,1>> inner_jpts;
-  std::vector<Eigen::Matrix<zsw::Scalar,3,1>> outer_jpts;
-  std::vector<Eigen::Matrix<zsw::Scalar,3,1>> bs_jpts;
-  zsw::genAndSampleShell(input_mesh, err_epsilon, tri_sample_r, inner_jpts, outer_jpts, bs_jpts);
-  zsw::writePoints(output_dir+"ori_inner_jpts.vtk", inner_jpts);
-  zsw::writePoints(output_dir+"ori_outer_jpts.vtk", outer_jpts);
-  zsw::writePoints(output_dir+"ori_bs_jpts.vtk", bs_jpts);
-  zsw::Approximation appro;
-  appro.setTmpOutDir(output_dir);
-  appro.init(err_epsilon, tri_sample_r, tet_sample_r, inner_jpts, outer_jpts, bs_jpts);
-  appro.writeTetMesh(output_dir+"refine_res.vtk", {zsw::ignore_bbox, zsw::ignore_self_in, zsw::ignore_self_out});
-  appro.simp(output_dir);
-  appro.writeTetMesh(output_dir+"simped_final.vtk", {zsw::ignore_bbox, zsw::ignore_out});
-}
-
-void appro_latest(const std::string &ori_file_path,
+void appro_v3(const std::string &ori_file_path,
                   const std::string &deformed_file_path,
                   const std::string &output_dir,
                   const zsw::Scalar err_epsilon,
@@ -168,10 +170,12 @@ void appro_latest(const std::string &ori_file_path,
 int main(int argc, char *argv[])
 {
   size_t v = atoi(argv[1]);
-  if(v == 0) {    appro_oriv(std::string(argv[2]), std::string(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7]));  }
-  else if(v <3 ) { appro_nowv(v, std::string(argv[2]), std::string(argv[3]), std::string(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7])); }
+  if(v == 0) {    appro_v0(std::string(argv[2]), std::string(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7]));  }
+  else if(v == 1) { appro_v1(v, std::string(argv[2]), std::string(argv[3]), std::string(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7])); }
   else if(v == 3) {
-    appro_latest(std::string(argv[2]), std::string(argv[3]), std::string(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7]), atoi(argv[8]));
+    appro_v3(std::string(argv[2]), std::string(argv[3]), std::string(argv[4]), atof(argv[5]), atof(argv[6]), atof(argv[7]), atoi(argv[8]));
+  } else {
+    std::cout << __FILE__ << __LINE__ << std::endl;
   }
   PRINT_FUNCTION_COST();
   return 0;
